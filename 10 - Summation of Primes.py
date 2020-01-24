e5277e4176e619ec0838b4f9beee3542d9392220
Fellenex <outcasthat@gmail.com>
2020-01-24 16:00:25 -0500

    Modded eratosthenes to use dicts, much faster. Added sieve of atkin, even faster! answer is wrong still for some reason, though

--- 10 - Summation of Primes.py
+++ 10 - Summation of Primes.py
@@ -61,30 +61,126 @@ def sieve_of_eratos_two(n):
 				break
 
 
+def sieve_of_eratos_three(n):
+	p = 2
+	carryOn = True
+	primeDict = {}
+
+	while (carryOn):
+		carryOn = False
+		for i in range(2, ((n/2)+1)):
+			if (p*i) <= n+1:
+				primeDict[(p*i)-1] = False
+				carryOn = True
+		for j in range(p+1, n):
+			try:
+				if primeDict[j-1] == False:
+					pass
+				else:
+					p = j
+					break
+			except KeyError:
+				p = j
+				break
+
+def sieve_of_atkin(n):
+	primeDict = {}
+	if n > 2: primeDict[2]=True
+	if n > 3: primeDict[3]=True
+
+	i = 1
+	while (i*i < n):
+		j = 1
+		while (j*j < n):
+			x = 4*(i*i) + (j*j)
+			if (x <= n and (x%12 == 1 or x%12 == 5)):
+				try:
+					#manual XOR since we don't initialize the list
+					if primeDict[x] == True:
+						primeDict[x] = False	#True XOR True is False
+					else:
+						primeDict[x] = True		#False XOR True is True
+				except KeyError:
+					#if it hasn't been indexed yet, then it means it's False
+					#	(the default value which would've been set)
+					#	so the XOR would resolve to True
+					primeDict[x] = True
+
+			x = 3*(i*i) + (j*j)
+			if (x <= n and x%12 == 7):
+				try:
+					if primeDict[x] == True:
+						primeDict[x] = False
+					else:
+						primeDict[x] = True
+				except KeyError:
+					primeDict[x] = True
+
+			x = 3*(i*i) - (j*j)
+			if (i > j and x <= n and x%12 == 11):
+				try:
+					if primeDict[x] == True:
+						primeDict[x] = False
+					else:
+						primeDict[x] = True
+				except KeyError:
+					primeDict[x] = True
+			j+=1
+		i+=1
+
+	r = 5
+	while (r*r < n):
+		try:
+			if primeDict[r]:
+				for i in range(r*r, n, r*r):
+					primeDict[i] = False
+		except KeyError:
+			#we were only removing multiples of squares.
+			#if it was never set, we don't need to reset it to false
+			pass
+		r+=1
+
+	primeSum = 0
+	for key in primeDict:
+		primeSum += key
+	print(primeSum)
+
 
 sum = 0
-x = 250
+x = 2000000
 numList = []
 primeList = []
 
 #This section of code compares the two Sieve functions. The second one runs slower with lower values
 #but it will dramatically increase speed as the size increases.
-#~ while (1):
-	#~ start = time.clock()
-	#~ sieve_of_eratos(x)
-	#~ finish = time.clock()
-	#~ print ("Sieve of Eratosthenes with {0} took: \t {1} time".format(x, (finish-start)))
+while (1):
+#	start = time.clock()
+#	sieve_of_eratos(x)
+#	finish = time.clock()
+#	print ("Sieve of Eratosthenes with {0} took: \t {1} time".format(x, (finish-start)))
 
-	#~ start = time.clock()
-	#~ sieve_of_eratos_two(x)
-	#~ finish = time.clock()
-	#~ print ("Sieve of Eratosth Two with {0} took: \t {1} time".format(x, (finish-start)))
+#	start = time.clock()
+#	sieve_of_eratos_two(x)
+#	finish = time.clock()
+#	print ("Sieve of Eratosth Two with {0} took: \t {1} time".format(x, (finish-start)))
 
-	#~ x *= 2
-	#~ if x > 16001:
-		#~ break
-sieve_of_eratos_two(2000000)
-for i in range(len(numList)):
-	if primeList[i] == True:
-		sum += numList[i]
-print sum
+	#start = time.clock()
+	#sieve_of_eratos_three(x)
+	#finish = time.clock()
+	#print ("Sieve of Eratosth Three with {0} took: \t {1} time".format(x, (finish-start)))
+
+	start = time.clock()
+	sieve_of_atkin(x)
+	finish = time.clock()
+	print ("Sieve of Atkin with {0} took: \t {1} time".format(x, (finish-start)))
+
+	if x > 2000000:
+		break
+
+	x *= 2
+
+#sieve_of_eratos_two(2000000)
+#for i in range(len(numList)):
+#	if primeList[i] == True:
+#		sum += numList[i]
+#print sum
