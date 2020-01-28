37a725d6f766a7dffe6b208ff14496a6d7eb83bd
Fellenex <outcasthat@gmail.com>
2020-01-28 12:31:26 -0500

    Solution to problem 3

--- 3 - Largest Prime Factor.py
+++ 3 - Largest Prime Factor.py
@@ -1,36 +1,36 @@
 #~ The prime factors of 13195 are 5, 7, 13 and 29.
 #~ What is the largest prime factor of the number 600851475143 ?
 
-def check_Prime(n):
-	for i in range(n-1, 1, -1):
-		if ((n%i) == 0):
-			return False
-	primeList.append(n)
-	return True
+from primeFuncs import *
 
-def find_Factors(n):
+def findFactors(n):
 	x = (n/2)+1
 	while (1):
 		if (x == 0):
 			break
 		if ((n % x) == 0):
-			factorsList.append(x)
+			return(x)
 		x -= 1
 
+def uniquePrimeFactorization(n):
+	primeFactors = []
+	i = 2
+	while i < (n/2)+1:
+		if n % i == 0:
+			if check_Prime(i):
+				print(str(i)+" is a prime factor, resetting target to "+str(n/i))
+				primeFactors.append(i)
 
-#This program works for smaller values, but it has not
-#shown to work for the value 600851475143.
-#This is because of the iterative nature of my algorithm
-primeList = []
-factorsList = []
-primeFactorsList = []
+				#reset the target and loop counter
+				n = n/i
+				i = 2
+		i+=1
+	#adjust for the final prime factor, for which no prime factors were found
+	primeFactors.append(n)
+	return(primeFactors)
 
-find_Factors(83950)
-largest = 0
-for i in range (len(factorsList)):
-	if check_Prime(factorsList[i]):
-		primeFactorsList.append(factorsList[i])
 
-primeFactorsList.reverse()
-print primeFactorsList
-	
+
+target = 600851475143
+
+print(uniquePrimeFactorization(target))
---
+++ primeFuncs.py
@@ -0,0 +1,139 @@
+#was going to use this function, but it has sluggish time
+#This function was stripped directly from problem 7, where it was
+#written to find the 10001st prime.
+def check_Prime(n):
+	print("Checking primality of "+str(n))
+	i = n-1
+	while (i > 1):
+		if ((n%i) == 0):
+			return False
+		i -= 1
+	return True
+
+
+#this version of the sieve function utilizes two arrays, one with
+#numbers, and one with True/False values (True indicating prime and
+#False indicating non-prime). It assumes they are prime, then works
+#to prove that numbers are not by removing all of the multiples of
+#other numbers
+def sieve_of_eratos_two(n):
+	primeList = []
+	numList = []
+
+	p = 2
+	carryOn = True
+	for i in range(1, n+1):
+		primeList.append(True)
+		numList.append(i)
+
+	#note, all values referenced in the primeList will be offset by -1 due to 0-based addressing
+	while (carryOn):
+		carryOn = False
+		for i in range(2, ((n/2)+1)):
+			if (p*i <= len(primeList)):
+				primeList[(p*i)-1] = False
+				carryOn = True
+		for j in range(p+1, n):
+			if (primeList[j-1] == True):
+				p = j
+				break
+
+	return(primeList)
+
+
+#More space-efficient version of eratosthenes' sieve
+#(using dictionaries this time, instead of giant lists)
+def sieve_of_eratos_three(n):
+	p = 2
+	carryOn = True
+	primeDict = {}
+
+	while (carryOn):
+		carryOn = False
+		for i in range(2, ((n/2)+1)):
+			if (p*i) <= n+1:
+				primeDict[(p*i)-1] = False
+				carryOn = True
+		for j in range(p+1, n):
+			try:
+				if primeDict[j-1] == False:
+					pass
+				else:
+					p = j
+					break
+			except KeyError:
+				p = j
+				break
+
+	return(primeDict)
+
+#An actually efficient sieve algorithm, modified
+#	to be more space-efficient with the use of dictionaries.
+#Finds all primes below supplied integer n.
+#Since we don't initialize the value of each integer,
+#	there is some sketchy repeated try/except code to calculate the XOR.
+#Factoring it out would mean passing the data structure around.
+def sieve_of_atkin(n):
+	primeDict = {}
+	if n > 2: primeDict[2]=True
+	if n > 3: primeDict[3]=True
+
+	i = 1
+	while (i*i < n):
+		j = 1
+		while (j*j < n):
+			x = 4*(i*i) + (j*j)
+			if (x <= n and (x%12 == 1 or x%12 == 5)):
+				try:
+					#manual XOR since we don't initialize the list
+					if primeDict[x] == True:
+						primeDict[x] = False	#True XOR True is False
+					else:
+						primeDict[x] = True		#False XOR True is True
+				except KeyError:
+					#if it hasn't been indexed yet, then it means it's False
+					#	(the default value which would've been set)
+					#	so the XOR would resolve to True
+					primeDict[x] = True
+
+			x = 3*(i*i) + (j*j)
+			if (x <= n and x%12 == 7):
+				try:
+					if primeDict[x] == True:
+						primeDict[x] = False
+					else:
+						primeDict[x] = True
+				except KeyError:
+					primeDict[x] = True
+
+			x = 3*(i*i) - (j*j)
+			if (i > j and x <= n and x%12 == 11):
+				try:
+					if primeDict[x] == True:
+						primeDict[x] = False
+					else:
+						primeDict[x] = True
+				except KeyError:
+					primeDict[x] = True
+			j+=1
+		i+=1
+
+	r = 5
+	while (r*r < n):
+		try:
+			if primeDict[r]:
+				for i in range(r*r, n, r*r):
+					primeDict[i] = False
+		except KeyError:
+			#we were only removing multiples of squares.
+			#if it was never set, we don't need to reset it to false
+			pass
+		r+=1
+
+	primesList = []
+	fakePrimes = []
+	for key in primeDict:
+		if primeDict[key]:
+			primesList.append(key)
+
+	return(primesList)
