077cbfe4fd5a68c236d0f8e7f70b8603249c2e96
Fellenex <outcasthat@gmail.com>
2020-01-28 14:37:09 -0500

    Prepping for p21

--- 21 - Amicable Numbers.py
+++ 21 - Amicable Numbers.py
@@ -3,7 +3,7 @@
 #~ If d(a) = b and d(b) = a, where a  b, then a and b are an amicable pair
 #~ and each of a and b are called amicable numbers.
 
-#~ For example, the proper divisors of 220 are 
+#~ For example, the proper divisors of 220 are
 #~ 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 #~ Evaluate the sum of all the amicable numbers under 10000.
@@ -35,13 +35,9 @@ totalSum = 0
 for i in range(1, 221):
 	amicableNum = sumFactors(i)
 	if (sumFactors(i) == sumFactors(amicableNum)):
-		
+
 		print i
 		print amicableNum
 		print sumFactors(i)
 		print sumFactors(amicableNum)
 		print
-		
-	
-		
-	
