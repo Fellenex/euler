5e0e009ae557153eabd84ca290b68e488695dbc4
Fellenex <outcasthat@gmail.com>
2020-02-07 14:37:52 -0500

    Updated single-pandigital-checking functions to include 'pandigitality' when zeroes are allowed, as in for p43.

--- pandigitalFuncs.py
+++ pandigitalFuncs.py
@@ -1,21 +1,37 @@
 pandigitalNum = 9
 
 #determines if a single number has any overlapping digits
-def isValidMult(_number, _pandigitality=pandigitalNum):
+def isValidMult(_number, _pandigitality=pandigitalNum, _noZeroesAllowed = True):
     strN = str(_number)
     validity = True
-    if '0' in strN:
+    if '0' in strN and _noZeroesAllowed:
         validity = False
     else:
         try:
-            cL = countList(_number, _pandigitality)
+            cL = countList(_number, _pandigitality, _noZeroesAllowed)
             for n in cL:
+                #if we have more than 1 of a digit, or 0 of a digit, then it is not pandigital
                 if n > 1 or n == 0:
                     validity = False
         except IndexError:
             validity=False
-            
+
     return(validity)
+
+#This function shouldn't be used on numbers containing 0s, since we are only looking to count integers from 1..n
+def countList(_number, _pandigitality=pandigitalNum, _noZeroesAllowed=True):
+    strN = str(_number)
+    if _noZeroesAllowed:
+        cList = [0]*_pandigitality
+        for n in strN:
+            cList[int(n)-1] += 1
+    else:
+        cList = [0]*(_pandigitality+1)
+        for n in strN:
+            cList[int(n)] += 1      #we don't want to offset the 1-counting to the 0-index if we include zeroes in pandigitality
+
+    return(cList)
+
 
 #determines if a list of numbers has any overlapping digits
 def isValidSequence(_numbers):
@@ -25,16 +41,6 @@ def isValidSequence(_numbers):
         if n > 1 or n == 0:
             validity = False
     return(validity)
-
-
-#This function shouldn't be used on numbers containing 0s, since we are only looking to count integers from 1..n
-def countList(_number, _pandigitality=pandigitalNum):
-    cList = [0]*_pandigitality
-    strN = str(_number)
-    for n in strN:
-        cList[int(n)-1] += 1
-    return(cList)
-
 
 #Takes a list of integers and creates a count list for the digits of each
 def countListMulti(_numbers):
