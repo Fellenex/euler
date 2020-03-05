6962a6e3691a9c15b537ef0a5b52565636f2163b
Fellenex <outcasthat@gmail.com>
2020-03-05 17:42:21 -0500

    Solution to p68. Generalized the solution to not assume where the smallest vertex would occur. Found all 4 solutions for a 16-character magic 5-gon.

--- 68 - Magic 5-gon.py
+++ 68 - Magic 5-gon.py
@@ -1,4 +1,4 @@
-solutions = []
+solutionStrings = []
 
 """
 since 10 has the smallest effect on the clockwise concatenation of 'gon-lines, we want it to be last
@@ -9,77 +9,63 @@ so then the vertex just clockwise of 10 is the smallest outside vertex (so that 
 --> v1 == 10 + a - c
 since v1 < 10, we get c > a
 
-v1 + b + c == v2 + c + d
---> v1 + b == v2 + d
---> v1 - v2 + b == d
---> v1 - v2 == d - b
-v1 is the smallest outside vertex, then v1 - v2 is negative
-then we get d - b is also negative, so b > d
-
-v2 + c + d == v3 + d + e
---> v2 + c == v3 + e
---> v2 - v3 + c == e
---> v2 - v3 == e - c
-since we want to maximize the string concatenation, v2 should be the largest outside vertex
-so then v2 > v3, meaning v2 - v3 is positive
-then e - c is also positive, so e > c
-
-v3 + d + e == v4 + e + a
---> v3 + d == v4 + a
---> v3 - v4 + d == a
---> v3 - v4 == a - d
-similarly with v2 and v3, v3 being larger has a greater effect on the string concatenation's total value
-so then v3 > v4, meaning v3 - v4 is positive
-then a - d is also positive, so a > d
-
 v4 + e + a == 10 + a + b
 --> v4 + e == 10 + b
---> v4 - 10 + e == b
 --> v4 - 10 == b - e
-since 10 is the maximum value, v4 must be smaller
-so then v4 - 10 is negative
-then b - e is also negative, so e > b
+since v4 < 10, we get e > b
 
-
-Combining these together, we get the following two properties:
-    e > c > a > d
-    e > b > d
-
-
-so one of the following must hold:
-    e > b > c > a > d
-    e > c > b > a > d
-    e > c > a > b > d
-
+We also get
+    v1 = 10 + a - c
+    v4 = 10 + b - e
 """
 
-#TODO: is it possible that we can't assume that 10 is the "final" line in the clockwise circulation?
+#always e > b
+for b in range(1,9):
+    for e in range(b+1,10):
 
-for vOne in range(1,6+1):
-    for vFour in range(vOne+1,7+1):
-        for vThree in range(vFour+1,8+1):
-            for vTwo in range(vThree+1,9+1):
+        #always c > a
+        for a in range(1,9):
+            for c in range(a+1,10):
 
-                for d in range(1,5+1):
-                    for e in range(d+4,9+1):
+                for d in range(1,10):
+                    insideAssignment = [a,b,c,d,e]
+                    unique = True
+                    for x in range(1,10):
+                        if insideAssignment.count(x) > 1:
+                            unique = False
 
-                        #The middle range, we don't know how b fits in with a,b, but we do know c > a
-                        #Even if we change a,b,c,d,e to be any value between [1,9], it still finds no solutions
-                        for b in range(2,8+1):
-                            for a in range(2,8+1):
-                                for c in range(a+1,8+1):
+                    if unique:
 
-                                    assignment = [vOne,vFour,vThree,vTwo,d,e,b,a,c]
-                                    unique = True
-                                    for x in range(1,10):
-                                        if assignment.count(x) > 1:
-                                            unique = False
+                        #We can calculate two of the outside vertex values (based on the inside vertex values)
+                        vOne = 10 + a - c
+                        vFour = 10 + b - e
 
-                                    if unique:
-                                        if (10+a+b) == (vOne+b+c) == (vTwo+c+d) == (vThree+d+e) == (vFour+e+a):
-                                            solutions.append(str(vOne)+str(b)+str(c)+str(vTwo)+str(c)+str(d)+str(vThree)+str(d)+str(e)+str(vFour)+str(e)+str(a)+"T"+str(a)+str(b))
+                        for vTwo in range(1,10):
+                            for vThree in range(1,10):
 
-print("Found "+str(len(solutions))+" solutions")
-for s in solutions[::-1]:
-    print([s[i*3:(i+1)*3] for i in range(5)])
-#All the solutions it's coming up with have duplicate elements throughout
+                                totalAssignment = [a,b,c,d,e,vOne,vTwo,vThree,vFour]
+                                unique = True
+                                for x in range(1,10):
+                                    if totalAssignment.count(x) > 1:
+                                        unique = False
+
+                                if unique:
+                                    if (10+a+b) == (vOne+b+c) == (vTwo+c+d) == (vThree+d+e) == (vFour+e+a):
+                                        solutionStrings.append(str(vOne)+str(b)+str(c)+str(vTwo)+str(c)+str(d)+str(vThree)+str(d)+str(e)+str(vFour)+str(e)+str(a)+"T"+str(a)+str(b))
+
+solutions = []
+print("Found "+str(len(solutionStrings))+" solutions")
+for s in solutionStrings[::-1]:
+    solutions.append([s[i*3:(i+1)*3] for i in range(5)])
+
+rotatedSolutions = []
+#Lucky for us, 'T' counts as larger than any numeric character.
+#Rotate the list so that the element with the smallest starting value (the outside vertex) is the first element.
+for s in solutions:
+    for e in range(len(s)):
+
+        if s[e] == min(s):
+            rotatedSolutions.append(s[e:]+s[:e])
+
+collapsedSolutions = sorted(["".join(rs) for rs in rotatedSolutions])
+print("The largest string for a magic 5-gon is "+str(collapsedSolutions[-1]))
