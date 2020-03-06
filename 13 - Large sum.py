6acd752a9e6a216123f6e05e257addd6a7b0c91b
Fellenex <outcasthat@gmail.com>
2020-03-05 22:42:49 -0500

    Removed erroneous p13

--- 13 - Large sum.py
+++
@@ -1,9 +0,0 @@
-#Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
-#See 13.txt for the numbers
-
-
-#Read in the numbers as strings
-rows=[]
-with open('13.txt', 'r') as f:
-    for line in f:
-        rows.append(line.rstrip('\n'))
