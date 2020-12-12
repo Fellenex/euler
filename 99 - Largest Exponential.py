b735484301d38d033f757c7ea51bdfff220f0e80
Fellenex <outcasthat@gmail.com>
2020-12-12 00:46:22 -0500

    Added some class compartmentalization and changed the double-indexing on all the bases and exponents.

--- 99 - Largest Exponential.py
+++ 99 - Largest Exponential.py
@@ -1,31 +1,35 @@
 from primeFuncs import gcd, coprime
 from math import log
 
+class ExpTuple:
+    def __init__(self, base, exp):
+        self.base = int(base)
+        self.exp = int(exp)
+
 expTuples = []
 with open('99.txt') as f:
     for line in f.readlines():
 
         #turns each line of the textfile into a 2-element tuple for exponent and base.
-        expTuples.append([int(x) for x in line.rstrip('\n').split(',')])
+        splitLine = line.rstrip('\n').split(',')
+        expTuples.append(ExpTuple(splitLine[0], splitLine[1]))
 
 
-#indices of the tuples
-BASE_INDEX = 0
-EXP_INDEX = 1
-
-maxTuple = (expTuples[0][BASE_INDEX], expTuples[0][EXP_INDEX])
-maxLineNumber = 1
+maxLineNumber = 1       #the project euler problem wants the line number of the maximal expression.
+#Initialize the maximum expression we've seen thus far.
+maxTuple = ExpTuple(expTuples[0].base, expTuples[0].exp)
 
 for i in range(1, len(expTuples)):
-
-    #Now we take the log of each number, taking expTuples[i] as a^b, and maxTuple as c^d.
-    l1 = expTuples[i][EXP_INDEX]        #log_a(a^(b)) == b
-    l2 = maxTuple[EXP_INDEX] * log(maxTuple[BASE_INDEX], expTuples[i][BASE_INDEX])      #log_a(c^(d)) == (d) * log_a(c)
+    #Now we take the log (base a) of each number, taking expTuples[i] as a^b, and maxTuple as c^d.
+    l1 = expTuples[i].exp        #log_a(a^(b)) == b
+    l2 = maxTuple.exp * log(maxTuple.base, expTuples[i].base)      #log_a(c^(d)) == (d) * log_a(c)
 
     if l1 > l2:
         #if the ith tuple is greater than the greatest we've seen so far, then change the greatest we've seen.
-        maxTuple = (expTuples[i][BASE_INDEX], expTuples[i][EXP_INDEX])
+        maxTuple = expTuples[i]
         maxLineNumber = i
 
-print(maxLineNumber)
-print(maxTuple)
+#We add one to the variable keeping track of the greatest expression's line number, since line count starts at 1.
+maxLineNumber += 1
+
+print("%s^%s is the maximal expression, appearing on line %s" % (maxTuple.base, maxTuple.exp, maxLineNumber))
