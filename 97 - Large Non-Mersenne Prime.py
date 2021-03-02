c98004174e5af3a81b74e5f23d42a586eb0f7738
Fellenex <outcasthat@gmail.com>
2021-03-02 18:50:27 -0500

    Cleaned up the code and generalized the 'get the last x digits of a big exponential' code into a function.

--- 97 - Large Non-Mersenne Prime.py
+++ 97 - Large Non-Mersenne Prime.py
@@ -1,4 +1,5 @@
 """
+P97
 The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
 
 However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×(2^7830457)+1.
@@ -6,25 +7,21 @@ However, in 2004 there was found a massive non-Mersenne prime which contains 2,3
 Find the last ten digits of this prime number.
 """
 
+precision = 10
+precisionPower = 10 ** precision        #remove digits using modular arithmetic
+
+def suffixingDigitsOfExponential(base, power, precision=3):
+    rollingDigits = base
+    for i in range(2, power+1):
+        rollingDigits = (rollingDigits * base) % precisionPower
+
+    print("The last %s digits of %s^%s are %s" % (precision, base, i, rollingDigits))
+    return(rollingDigits)
 
 k = 28433
 power = 7830457
 base = 2
 
-precision = 10
-precisionPower = 10 ** precision        #remove digits using modular arithmetic
-
-rollingDigits = base
-i = 2
-while i <= power:
-    rollingDigits = (rollingDigits * base) % precisionPower
-    i +=1
-
-print("The last %s digits of %s^%s are %s" % (precision, base, i, rollingDigits))
-
-#multiply by the factor, and the added constant
-rollingDigits = (rollingDigits * k) % precisionPower
-
-rollingDigits = (rollingDigits + 1) % precisionPower
-
-print("The last %s digits of %s*(%s^%s)+1 are %s" % (precision, k, base, power, rollingDigits))
+n = suffixingDigitsOfExponential(base,power,precision)
+n = ((k * n)+1) % precisionPower
+print(n)
